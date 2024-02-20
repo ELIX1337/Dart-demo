c16f4200e20d23df05e6bf0371c16e3521017ef5
Tim <timur-sibay@mail.ru>
2024-02-20 19:32:46 +0500

    Changed copyright header

--- lib/periphy.dart
+++ lib/periphy.dart
@@ -1,6 +1,6 @@
-// Copyright 2017-2023, Tim Yumalin.
+// Copyright 2024, Tim Yumalin.
 // All rights reserved. Use of this source code is governed by a
-// BSD-style license that can be found in the LICENSE file.
+// MIT-style license that can be found in the LICENSE file.
 
 library periphy;
 
